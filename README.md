996c7cbc1cafccb5234ba66260a6bb67cfaefee7
Simon Blackwell <syblackwell@anywhichway.com>
2017-08-08 22:21:24 -0400

    Synchronizing now working with `storeserver`. Also tested with window.localStorage and `localdbstore`, which is a wrapper for IndexedDb.

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Offline first IndexedDB storage providing async version of Web Storage API that 
 
 # Release History (reverse chronological order)
 
+v0.0.2 2017-08-08 ALPHA Synchronizing now working with `storeserver`. Also tested with window.localStorage and `localdbstore`, which is a wrapper for IndexedDb.
+
 v0.0.1 2017-07-31 ALPHA Initial public release
--- index.js
+++ index.js
@@ -1,259 +1,192 @@
 (function() {
 	"use strict";
-
-	function OffLineFirst(name,remote,options={clear:false}) {
+	
+	const LocalStorage = new Proxy(window.localStorage,{get: (target,property) => {
+		if(property==="get") return async (id) => JSON.parse(target.getItem(id));
+		if(property==="set") return async (id,data) => target.setItem(id,JSON.stringify(data));
+		if(property==="delete") return async (id) => target.removeItem(id);
+		if(property==="count") return async () => target.length;
+		return target[property];
+	}});
+	
+	// hand in a Web Storage API compatible local store and remote store, options for server win or local win
+	// otherwise wrap local store in versionedstore
+	function OffLineFirst(local,remote,options={clear:false}) { // keyPath:"_.#"
 		let resolver, rejector, objectstore;
-		this.location = remote || null;
-		this.opened = new Promise((resolve,reject) => { resolver = resolve; rejector = reject;});
-		const me = this,
-			request = indexedDB.open(name);
-		request.onupgradeneeded = (event) => {
-			me.indexed = event.target.result;
-			objectstore = me.indexed.createObjectStore("keyvaluepairs", { keyPath: "id" });
-		}
-		request.onsuccess = (event) => {
-			if(objectstore) {
-				objectstore.transaction.complete = (event) => {
-					if(options.clear) {
-						me.clear().then(() => {
-							resolver();
-						})
+		this.options = {...options};
+		this.options.clear = (typeof(options.clear)==="boolean" ? options.clear : false);
+		this.local = local || LocalStorage;
+		this.remote = remote;
+		this.options.futures = (typeof(options.futures)==="number" ? options.futures : 5000);
+		this.versionManager = options.versionManager;
+		const me = this;
+		this.get("OfflineFutureQueue",-1).then(async (queue) => {
+			if(queue) {
+				const now = Date.now();
+				for(let key in queue) {
+					const command = queue[key],
+						data = command.arguments[1];
+						touched = data[this.versionManager.metadataPath()]["@"];
+					if(touched<=now) {
+						await me[command.apply].apply(this,command.arguments);
 					} else {
-						resolver();
-					}	
-				}
-			} else {
-				me.indexed = event.target.result;
-				if(options.clear) {
-					me.clear().then(() => {
-						resolver();
-					})
-				} else {
-					resolver();
+						setTimeout(async () => {
+							await me[command.apply].apply(this,command.arguments);
+							queue = await me.get("OfflineFutureQueue",-1);
+							delete queue[key];
+							await me.set("OfflineFutureQueue",-1);
+						},touched - now);
+					}
 				}
 			}
-		}
+		});
+		
 		return new Proxy(this,{get: (target,property) => {
 			if(typeof(target[property])!=="undefined") {
 				return target[property];
 			} else {
-				return async function() {
-					return target.apply(property,[].slice.call(arguments));
-				}
+				return remote[property];
 			}
 		}});
 	}
-	OffLineFirst.prototype.apply = async function(name,args=[]) {
-		return this.fetch(this.location+"/"+name,{method:"POST",body:args})
-	}
-	OffLineFirst.prototype.clear = async function(local) {
-		local || (local = (this.location==null));
-		const me = this;
-		return new Promise((resolve,reject) => {
-			const transaction = me.indexed.transaction(["keyvaluepairs"],"readwrite"),
-				objectstore = transaction.objectStore("keyvaluepairs"),
-				request = objectstore.clear();
-			request.onsuccess = () => {
-				if(!local) {
-					me.fetch(me.location+"/clear",{method:"POST"})
-						.then(() => {
-							resolve();
-						})
-						.catch((e) => {
-							if(e instanceof TypeError) {
-								resolve();
-							} else {
-								throw(e);
-							}
-						});
-				} else {
-					resolve();
-				}
-			}
-		});
-	}
-	OffLineFirst.prototype.count = async function(local) {
-		local || (local = (this.location==null));
-		const me = this;
-		return new Promise((resolve,reject) => {
-			const transaction = me.indexed.transaction(["keyvaluepairs"],"readwrite"),
-				objectstore = transaction.objectStore("keyvaluepairs"),
-				request = objectstore.count();
-			request.onsuccess = () => {
-				let result = request.result;
-				if(!local) {
-					me.fetch(me.location+"/count",{method:"POST"})
-						.then(async (response) => {
-							if(response.status===200) {
-								try {
-									result = await response.json();
-								} catch(e) { // result is undefined
-									;
-								}
-							}
-							resolve(result);
-						})
-						.catch((e) => {
-							if(e instanceof TypeError) {
-								resolve(result);
-							} else {
-								throw(e);
-							}
-						});
-				} else {
-					resolve(result);
-				}
-			}
-		});
-	}
-	OffLineFirst.prototype.delete = async function(id,local) {
-		local || (local = (this.location==null));
-		const me = this;
-		return new Promise((resolve,reject) => {
-			const transaction = me.indexed.transaction(["keyvaluepairs"],"readwrite"),
-				objectstore = transaction.objectStore("keyvaluepairs"),
-				request = objectstore.count();
-			request.onsuccess = () => {
-				if(!local) {
-					fetch(me.location+"/"+id,{method:"DELETE"})
-						.then(() => {
-							resolve();
-						})
-						.catch((e) => {
-							if(e instanceof TypeError) {
-								resolve();
-							} else {
-								throw(e);
-							}
-						})
-				} else {
-					resolve();
-				}	
-			}
-			request.onerror = () => {
-				if(!local) {
-					fetch(me.location+"/"+id,{method:"DELETE"})
-						.then(() => {
-							resolve();
-						})
-						.catch((e) => {
-							if(e instanceof TypeError) {
-								resolve();
-							} else {
-								throw(e);
-							}
-						})
-				} else {
-					resolve();
-				}
-			}
-		})
-	}
-	OffLineFirst.prototype.removeItem = OffLineFirst.prototype.delete;
-	OffLineFirst.prototype.fetch = async function(url,options) {
-		const me = this;
-		me.processQueue();
-		return fetch(url,options).then((response) => {
-			return response;
-		}).catch((e) => {
-			if(e instanceof TypeError) {
-				me.queue || (me.queue = []);
-				me.queue.push({url,options});
-				me.set("OfflineFirstQueue",me.queue,true);
-			}
-			throw e;
-		})
-	}
-	OffLineFirst.prototype.processQueue = async function() {
-		let queue = await this.get("OfflineFirstQueue",true);
-		queue || (queue = []);
-		this.queue = queue;
-		for(let command of queue) {
+	OffLineFirst.prototype.clear = async function(locations=0) {
+		locations>0 || await this.local.clear();
+		if(locations>=0) {
 			try {
-				await fetch(command.url,command.options);
-				queue.shift();
-				await this.set("OfflineFirstQueue",queue,true);
+				await this.remote.clear();
 			} catch(e) {
-				if(e instanceof TypeError) {
-					return;
+				if(e.message!=="Failed to fetch") {
+					throw(e);
 				}
-				throw(e);
 			}
 		}
 	}
-	OffLineFirst.prototype.get = async function(id,local) {
-		local || (local = (this.location==null));
+	OffLineFirst.prototype.count = async function(locations=0) {
+		let count;
+		locations>0 || (count = await this.local.count());
+		if(locations>=0) {
+			try {
+				count = await this.remote.count();
+			} catch(e) {
+				if(e.message!=="Failed to fetch") {
+					throw(e);
+				}
+			}
+		}
+		return count;
+	}
+	OffLineFirst.prototype.enqueue = async function(command,args) {
+		this.queue || (this.queue = await this.get("OfflineRemoteQueue",-1)) || (this.queue = {});
+		this.queue[uuidv4()] = {apply:command,arguments:args};
+		await this.set("OfflineRemoteQueue",this.queue,-1);
+	}
+	OffLineFirst.prototype.delete = async function(id,locations=0) {
+		locations>0 || await this.local.delete(id);
+		if(locations>=0) {
+			try {
+				await this.remote.delete(id);
+			} catch(e) {
+				if(e.message==="Failed to fetch") {
+					await this.enqueue("delete",[id,1]); // this will delete the local copy, not good, need to reqork the local/remote seecond are of all commands
+				} else {
+					throw(e);
+				}
+			}
+		}
+	}
+	OffLineFirst.prototype.removeItem = OffLineFirst.prototype.delete;
+	OffLineFirst.prototype.future = async function(apply,args,delay) {
+		queue || (queue = await this.get("OfflineFutureQueue",-1)) || (queue = {});
+		const me = this,
+			metadatapath = this.versionManager.metadataPath(),
+			value = {...data},
+			id = uuidv4();
+		value[metadatapath] = data[metadatapath];
+		queue[id] = {apply,arguments:args};
+		await this.set("OfflineFutureQueue",queue,-1);
+		setTimeout(async () => {
+			const queue = await me.get("OfflineFutureQueue",-1);
+			await me[command.apply].apply(me,command.arguments);
+			delete queue[id];
+			await me.set("OfflineFutureQueue",-1);
+		},delay);
+	}
+	OffLineFirst.prototype.synchronize = async function() {
+		const queue = this.queue || (this.queue = await this.get("OfflineRemoteQueue",-1)) || (this.queue = {});
+		for(let id in queue) {
+			const command = queue[id];
+			if(command) {
+				await this.remote[command.apply].apply(this,command.arguments);
+				delete queue[id];
+				await this.set("OfflineRemoteQueue",queue,-1);
+			}
+		}
+		return true;
+	}
+	OffLineFirst.prototype.get = async function(id,locations=0) {
 		const me = this;
-		return this.opened.then(() => {
-			return new Promise((resolve,reject) => {
-				const promise = (local ? Promise.reject(new TypeError()) : this.fetch(this.location+"/"+id,{method:"GET"}))
-				return promise.then(async (response) => {
-						if(response.status===200) {
-							let result;
-							try {
-								result = await response.json();
-							} catch(e) { // result is undefined
-								//await me.delete(id,true);
-								resolve();
-								return;
-							}
-							// add to local database
-							await me.set(id,result,true);
-							resolve(result);
+		let value = await this.local.get(id);
+		if(locations>=0) {
+			try {
+				this.synchronize();
+				const remotevalue = await this.remote.get(id);
+				if(value && remotevalue) {
+					const now = Date.now(),
+						metadata =  remotevalue[this.versionManager.metadataPath()];
+					if(metadata) {
+						const touched = metadata["@"];
+						if(touched>now) {
+							await this.future("set",[id,value,-1,true],touched - now);
 						} else {
-							throw new TypeError(response.status);
+							await this.set(id,this.versionManager.mediate(value,remotevalue),-1);
 						}
-					})
-					.catch((e) => { 
-						if(e instanceof TypeError) {
-							const transaction = this.indexed.transaction(["keyvaluepairs"],"readwrite"),
-								objectstore = transaction.objectStore("keyvaluepairs"),
-								request = objectstore.get(id);
-							request.onsuccess = (event) => {
-								resolve(event.target.result.data);	
-							}
-							request.onerror = (event) => {
-								reject(JSON.stringify(event));
-							}
-						} else {
-							reject(e);
-						}
-					});
-			});
-		});
+					}
+				}
+			} catch(e) {
+				if(e.message==="Failed to fetch") {
+					await this.enqueue("get",[id]);
+				} else {
+					throw(e);
+				}
+			}
+		}
+		return value;
 	}
 	OffLineFirst.prototype.getItem = OffLineFirst.prototype.get;
 	OffLineFirst.prototype.key = async function(number) {
 		return await this.apply("key",[number]);
 	}
-	OffLineFirst.prototype.set = async function(id,data,local) {
-		local || (local = (this.location==null));
-		const me = this;
-		return this.opened.then(() => {
-			return new Promise((resolve,reject) => {
-				const transaction = me.indexed.transaction(["keyvaluepairs"],"readwrite"),
-					objectstore = transaction.objectStore("keyvaluepairs"),
-					request = objectstore.put({id,data});
-				request.onsuccess = (event) => {
-					if(local) {
-						resolve();
-					} else {
-						return me.fetch(me.location+"/"+id,{method:"PUT",body:data})
-							.then(() => resolve())
-							.catch((e) => { 
-								if(e instanceof TypeError) {
-									resolve();
-								} else {
-									reject(e);
-								}
-							})
-					}	
+	OffLineFirst.prototype.put = async function(data,locations=0) {
+		const parts = this.versionManager.keyPath().split(".");
+		this.versionManager.baseline(data);
+		let node = data, id;
+		for(let i=0;i<parts.length;i++) {
+			const part = parts[i];
+			id = node = node[part];
+		}
+		return this.set(id,data,locations);
+	}
+	OffLineFirst.prototype.putItem = OffLineFirst.prototype.put;
+	OffLineFirst.prototype.set = async function(id,data,locations=0,mediate) {
+		if(mediate) {
+			await this.versionManager.mediate(data,this.get(id,-1));
+		}
+		const metadatapath = this.versionManager.metadataPath(),
+			value = {...data};
+		value[metadatapath] = data[metadatapath];
+		locations>0 || await this.local.set(id,value);
+		if(locations>=0) {
+			try {
+				this.synchronize();
+				await this.remote.set(id,value);
+			} catch(e) {
+				if(e.message==="Failed to fetch") {
+					await this.enqueue("set",[id,value,1]);
+				} else {
+					throw(e);
 				}
-				request.onerror = (event) => {
-					reject(JSON.stringify(event));
-				}
-			});
-		});
+			}
+		}
 	}
 	OffLineFirst.prototype.setItem = OffLineFirst.prototype.set;
 
